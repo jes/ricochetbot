fe81cf24d8ef52c5e5729644781220a644fa143c
James Stanley <james@incoherency.co.uk>
2018-09-27 01:18:16 +0100

    Open an outbound channel as soon as a peer connects, so that we can always send messages to them

--- contactmanager.go
+++ contactmanager.go
@@ -16,6 +16,10 @@ func (rbcm *RicochetBotContactManager) LookupContact(hostname string, publicKey 
 	status := true
 	if rbcm.bot.OnNewPeer != nil {
 		status = rbcm.bot.OnNewPeer(rbcm.bot.LookupPeerByHostname(hostname))
+		// XXX: call the handler for when an inbound channel is opened, this is a bodge so that the bot can
+		// open an outbound channel immediately if it wants to; having an outbound channel always open makes
+		// it possible to send messages to a peer without having to first remember to open the outbound channel
+		go rbcm.bot.LookupPeerByHostname(hostname).OpenInbound()
 	}
 	return status, status
 }
